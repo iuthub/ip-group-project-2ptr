e21657489bad75acde3185d92ed814abd6d89e91
gulomov <gulomov.98@bk.ru>
2020-05-06 00:27:46 +0500

    updated

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -28,17 +28,17 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
+    sales\Http\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
+   sales\Console\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
+   sales\Exceptions\Handler::class
 );
 
 /*
