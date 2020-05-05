a1a21f2b2f5deec088612ad9a7dc2651a8f1a0b6
gulomov <gulomov.98@bk.ru>
2020-05-05 23:40:06 +0500

    updated

--- laravel/app/Providers/EventServiceProvider.php
+++ laravel/app/Providers/EventServiceProvider.php
@@ -1,11 +1,9 @@
 <?php
 
-namespace App\Providers;
+namespace sales\Providers;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -15,8 +13,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        'sales\Events\Event' => [
+            'sales\Listeners\EventListener',
         ],
     ];
 
